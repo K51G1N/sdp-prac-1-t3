ce0d359998c05c61481c701e11ed144e0572a81a
vuyanitutwane <vuyaitutwane@outlook.com>
2020-02-15 19:55:22 +0200

    Solved a problem that requires a total sum of all multiples of 3 or 5 below 100

--- Euler.py
+++ Euler.py
@@ -1,4 +1,16 @@
-def main():
 
+'''
+*****17t4564****
+Finding the sum of all the multiples of 3 or 5 below 1000
+'''
+
+def main():
+    prime = 0 
+    for i in range(0,1000):
+        if i%3==0 or i%5==0:
+            prime+=i
+    print (prime)
+    
+#When the main function is executed, it adds all the numbers (from 0 to 1000) that are divisible by either 3 or 5 and then prints out the total sum.
 
 main()
