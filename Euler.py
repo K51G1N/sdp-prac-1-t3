3736a44fa1d75dcf26d35e290dad4d494ada8bef
Power-Circuit <phirikhumbolane@gmail.com>
2020-02-17 22:17:19 +0200

    I have changed my solution to Euler solution 1

--- Euler.py
+++ Euler.py
@@ -42,46 +42,40 @@ def SumSquares(n): #g17e4476 Calculates the Sum of the Squares and returns the a
         n -= 1
     return sumsq
         
-def sumOfPRimes(n): #g19p6350 Calcultes the sum of all the primes 
-	sumprime = 0;
-	for num in range(1, n):
-            if num > 1:
-               for i in range(2, num):
-                   if (num % i) == 0:
-                       break
-                   else:
-                       sumprime += num
-                       
-	return sumprime	   
-
+def ThreesAndFives(n): #g19p6350 Calculates the sum of multiples of 3 and 5
+    
+    total = 0
+    for num in range(n):
+        if num % 3 == 0 or num % 5 == 0:
+            total += num
+            
+    return total
+   
 def SumSquareDiff(n):
    return(SquareSum(n)-SumSquares(n)) #g17e4476 returns the difference between
   
   
 def main():
-	#g17e4476
+    #g17e4476
     p6sol = SumSquareDiff(100)  #g17e4476 Solved project Euler 6, 'https://projecteuler.net/problem=6'
     print("Project Euler, Problem 6 solution: ")
     print(p6sol) #g17e4476 Solution Output
-	
-	#17t4564
+    
+    #17t4564
     prime = 0 
     for i in range(0,1000):
         if i%3==0 or i%5==0:
             prime+=i
     print (prime)
-	#When the main function is executed, it adds all the numbers (from 0 to 1000) that are divisible by either 3 or 5 and then prints out the total sum.
-	# g18m6734 made this
-	# for the second Euler Problem
+    #When the main function is executed, it adds all the numbers (from 0 to 1000) that are divisible by either 3 or 5 and then prints out the total sum.
+    # g18m6734 made this
+    # for the second Euler Problem
     dat = fib()
     get = even(dat)
     a = add(get)
-    print (a)
+    g = ThreesAndFives(a) #19p6350 solved Euler problem 1
+    print (g)
     
-    #g19p6350 testing sumprime function
-    print("sum prime is: " )
-    print(sumOfPRimes(5))
-
-
+   
 main()
 
